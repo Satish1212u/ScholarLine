b0b3bf9655d57dccb45c0821012703d30b3dedc0
Satish Gupta <gargshaurya2005@gmailcom>
2026-05-07 13:47:43 +0530

    Fixed sitemap configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://scholar-line.com'
+    }),
   ],
 })
